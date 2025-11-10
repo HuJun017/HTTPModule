84971970be94490a14239ddf1d6c43a2abb1b6cc
HuJun017 <166590848+HuJun017@users.noreply.github.com>
2025-11-10 12:36:36 +0000

    checkpoint

---
+++ HTTPModule/src/app/app.ts
@@ -0,0 +1,13 @@
+import { HttpClientModule } from '@angular/common/http';
+import { Component, signal } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, RouterLink],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('HTTPModule');
+}
